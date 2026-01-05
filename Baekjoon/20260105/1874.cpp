0d528c98d077a948b4a5e9e96bae9c3c7196b01a
hyjnkkr <therealguy03@gmail.com>
2026-01-05 14:55:27 +0900

    0105

--- Baekjoon/20260105/1874.cpp
+++ Baekjoon/20260105/1874.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 
 using namespace std;
 
 int arr[100001];
 stack<int> a;
+vector<char> v;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-    int n;
-    cin >> n;
-
-    for(int i = 0; i < n; i++) {
-        
-    }
     
 
 	return 0;
