7136c1500405d8ed78aacb3eb8e950a943c46c3b
hyjnkkr <therealguy03@gmail.com>
2026-01-10 11:51:39 +0900

    9935

--- Baekjoon/20260105/9935.cpp
+++ Baekjoon/20260105/9935.cpp
@@ -3,45 +3,48 @@
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	string orig, exp; // original / explosion target
-	string res = ""; // the final  result
-	cin >> orig >> exp;
+  string orig, exp; // original / explosion target
+  string res = "";  // the final  result
+  cin >> orig >> exp;
 
-	int orig_len = orig.length(); int exp_len = exp.length();
+  int orig_len = orig.length();
+  int exp_len = exp.length();
 
-	// from i = 0 -> orig_len, push the items into 'result' one by one.
-	// if the 'last' character is pushed, start checking whether
-	// the explosion would occur.
+  // from i = 0 -> orig_len, push the items into 'result' one by one.
+  // if the 'last' character is pushed, start checking whether
+  // the explosion would occur.
 
-	for (int i = 0; i < orig_len; i++) {
-	    res.push_back(orig[i]);
-		// explosion seq (last character is pushed && result's size bigger than exp's length)
-		if (orig[i] == exp[exp_len - 1] && res.size() >= exp_len) {
+  for (int i = 0; i < orig_len; i++) {
+    res.push_back(orig[i]);
+    // explosion seq (last character is pushed && result's size bigger than
+    // exp's length)
+    if (orig[i] == exp[exp_len - 1] && res.size() >= exp_len) {
 
-		    // check if the exp (completely same) exists
-		    bool match = true;
-			for (int j = 0; j < exp_len; j++) {
-			    if (res[res.size() - exp_len + j] != exp[j]) {
-					match = false;
-					break;
-				}
-			}
+      // check if the exp (completely same) exists
+      bool match = true;
+      for (int j = 0; j < exp_len; j++) {
+        if (res[res.size() - exp_len + j] != exp[j]) {
+          match = false;
+          break;
+        }
+      }
 
-			if (match == true) {
-			    for (int j = 0; j < exp_len; j++) {
-					res.pop_back();
-				}
-			}
-		}
-	}
+      if (match == true) {
+        for (int j = 0; j < exp_len; j++) {
+          res.pop_back();
+        }
+      }
+    }
+  }
 
-	if (res.empty()) {
-	    cout << "FRULA" << '\n';
-	} else {
-	    cout << res << '\n';
-	}
-	return 0;
+  if (res.empty()) {
+    cout << "FRULA" << '\n';
+  } else {
+    cout << res << '\n';
+  }
+  return 0;
 }
